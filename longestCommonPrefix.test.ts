9bfcdfc13f84853a24900821e3a0f8a673eeb3d2
Itamar Tati <itamar.tati1994@gmail.com>
2020-09-21 10:49:55 +0100

    changed test decribtion

--- longestCommonPrefix.test.ts
+++ longestCommonPrefix.test.ts
@@ -1,6 +1,6 @@
 const LongestCommonPrefix = require('./longestCommonPrefix');
 
-test('check capitalisation', () => {
+test('find the longest prefix', () => {
     expect(LongestCommonPrefix(["colorado", "color", "cold"])).toBe('col');
     expect(LongestCommonPrefix(["a", "b", "c"],)).toBe('');
     expect(LongestCommonPrefix(["spot", "spotty", "spotted"])).toBe('spot');
